9c9931e393bc360db04faba66321050df6a10a7d
Danny Vaca <danny270793@icloud.com>
2025-05-17 10:04:02 -0500

    show tree

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import { Parser, Tree } from "./libraries/parser"
 import { rules } from "./libraries/rules"
 import { Token, Tokenizer } from "./libraries/tokenizer"
 
-const expression: string = '12+23'
+const expression: string = '12+(23-45)/67*89'
 
-const characters: Character[] = Characterizer.characterize(expression, 'std::in')
 console.log('Characters:')
+const characters: Character[] = Characterizer.characterize(expression, 'std::in')
 characters.forEach((character: Character) => {
     console.log(character)
 })
 
+console.log('\nTokens:')
 const tokens: Token[] = Tokenizer.tokenize(characters, rules)
-console.log('Tokens:')
 tokens.forEach((token: Token) => {
     console.log(token)
 })
 
+console.log('\nTree:')
 const tree: Tree = Parser.parse(tokens)
-console.log('tree:')
-console.log(tree)
+console.log(JSON.stringify(tree, null, 2))
