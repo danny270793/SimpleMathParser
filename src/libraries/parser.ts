09e2ecced8bd1739ae6a4dd85fd4dc681322553b
Danny Vaca <danny270793@icloud.com>
2025-05-16 21:32:55 -0500

    create empty parser

---
+++ src/libraries/parser.ts
@@ -0,0 +1,13 @@
+import { Token } from "./tokenizer";
+
+export interface Tree {
+
+}
+
+export class Parser {
+    static parse(tokens: Token[]): Tree {
+        const tree: Tree = {}
+
+        return tree
+    }
+}
